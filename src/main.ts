098c05ea4d3fd1899dee98aca641980f313e0111
itmysm <dev.mysm@gmail.com>
2024-10-28 18:44:17 +0330

    feat: added Bootstrap and created a custom button component

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import './assets/styles/main.css'
+import './assets/styles/customized-bootstrap/index.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
---
+++ src/types/bootstrapTypes.ts
@@ -0,0 +1,26 @@
+export type ButtonVariant =
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'danger'
+  | 'warning'
+  | 'info'
+  | 'light'
+  | 'dark'
+  | 'outline-primary'
+  | 'outline-secondary'
+  | 'outline-success'
+  | 'outline-danger'
+  | 'outline-warning'
+  | 'outline-info'
+  | 'outline-light'
+  | 'outline-dark';
+
+export type ButtonType =
+  | 'button'
+  | 'submit'
+  | 'reset'
+
+export type ButtonSize =
+  | "lg"
+  | "sm"
