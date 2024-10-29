6769eb67270f0da8c92122b5859c496c463c6d3b
itmysm <dev.mysm@gmail.com>
2024-10-29 16:03:01 +0330

    feat: wrapped both login and register pages into one parent component

---
+++ src/components/pages/auth/AuthCardWrapper.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex justify-content-center align-items-center min-vh-100">
+    <div class="card px-4 pt-4 login-card shadow border-none">
+      <h2 class="fs-1 text-center text-gray">
+        {{ isLogin ? 'Login' : 'Register' }}
+      </h2>
+      <slot />
+
+      <p class="text-start mt-3">
+        {{ isLogin ? 'Don’t have account?' : 'Already Registered?' }}
+        <a
+          :href="`/auth/${isLogin ? 'register' : 'login'}`"
+          class="text-charcoal-grey fw-bold text-decoration-none link-opacity-25-hover"
+        >
+          {{ isLogin ? 'Register Now' : 'Login' }}
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  isLogin: {
+    type: Boolean,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.login-card {
+  max-width: 450px;
+  width: 100%;
+  border: none;
+}
+</style>
--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,59 +1,46 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center min-vh-100">
-    <div class="card px-4 pt-4 login-card shadow border-none">
-      <h2 class="fs-1 text-center text-gray">LOGIN</h2>
-      <form>
-        <div class="mb-3">
-          <label for="email" class="form-label text-charcoal-grey fw-semibold"
-            >Email</label
-          >
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            required
-            v-model="loginModel.email"
-          />
-        </div>
-        <div class="mb-3">
-          <label
-            for="password"
-            class="form-label text-charcoal-grey fw-semibold"
-            >Password</label
-          >
-          <input
-            type="password"
-            class="form-control is-invalid"
-            id="password"
-            v-model="loginModel.password"
-            required
-          />
-          <div class="invalid-feedback">Required field</div>
-        </div>
-        <button
-          :class="`btn btn-primary w-100 text-white mt-3 py-2`"
-          :disabled="!isFormValid || loading.login"
-          @click="onHandleLogin"
+  <AuthCardWrapper :is-login="true">
+    <form>
+      <div class="mb-3">
+        <label for="email" class="form-label text-charcoal-grey fw-semibold"
+          >Email</label
         >
-          <div
-            v-show="loading.login"
-            class="spinner-border text-white spinner-border-sm"
-            role="status"
-          ></div>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          required
+          v-model="loginModel.email"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label text-charcoal-grey fw-semibold"
+          >Password</label
+        >
+        <input
+          type="password"
+          class="form-control is-invalid"
+          id="password"
+          v-model="loginModel.password"
+          required
+        />
+        <div class="invalid-feedback">Required field</div>
+      </div>
+      <button
+        :class="`btn btn-primary w-100 text-white mt-3 py-2`"
+        :disabled="!isFormValid || loading.login"
+        @click="onHandleLogin"
+      >
+        <div
+          v-show="loading.login"
+          class="spinner-border text-white spinner-border-sm"
+          role="status"
+        ></div>
 
-          <span v-show="!loading.login">Login</span>
-        </button>
-      </form>
-      <p class="text-start mt-3">
-        Don’t have an account?
-        <a
-          href="/auth/register"
-          class="text-charcoal-grey fw-bold text-decoration-none link-opacity-25-hover"
-          >Register Now</a
-        >
-      </p>
-    </div>
-  </div>
+        <span v-show="!loading.login">Login</span>
+      </button>
+    </form>
+  </AuthCardWrapper>
 </template>
 
 <script setup>
@@ -61,6 +48,7 @@ import { reactive, computed, ref } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 import { useAlertStore } from '@/stores/alert'
 import { useRouter } from 'vue-router'
+import AuthCardWrapper from '@/components/pages/auth/AuthCardWrapper.vue'
 
 const alertStore = useAlertStore()
 const { push } = useRouter()
@@ -84,8 +72,7 @@ const onHandleLogin = async () => {
   loading.login = true
 
   try {
-    const response = await login({ ...loginModel })
-    await onHandleSaveSession()
+    Promise.all([await login({ ...loginModel }), await onHandleSaveSession()])
 
     alertStore.showAlert(
       'Login was successful! Redirecting to home page',
@@ -93,10 +80,8 @@ const onHandleLogin = async () => {
     )
     push('/')
   } catch (error) {
-    alertStore.showAlert(
-      'Login Failed!  User name and/or Password is invalid',
-      'danger',
-    )
+    const errorMessages = error.response?.data?.message
+    alertStore.showAlert(errorMessages || 'Can not reach the server', 'danger')
   } finally {
     loading.login = false
   }
@@ -106,11 +91,3 @@ const onHandleSaveSession = async () => {
   await saveLoginSession()
 }
 </script>
-
-<style lang="scss" scoped>
-.login-card {
-  max-width: 450px;
-  width: 100%;
-  border: none;
-}
-</style>
